2d775cee1ddfa25c6b1e8399ef9820bba4da90ea
nashcft <nashrfv@gmail.com>
2024-04-15 17:09:11 +0900

    chapter 4: do refactoring to_hex

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,6 +1,5 @@
 def to_hex(a, b, c)
-    "#" +
-    a.to_s(16).rjust(2, "0") +
-    b.to_s(16).rjust(2, "0") +
-    c.to_s(16).rjust(2, "0")
+  [a, b, c].sum("#") do |n|
+    n.to_s(16).rjust(2, "0")
+  end
 end
