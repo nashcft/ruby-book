cfc725cddc1d6ff36cbad87e697253a7503c6f32
nashcft <nashrfv@gmail.com>
2024-04-15 17:50:01 +0900

    chapter 4: do refactoring to_ints

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -4,9 +4,4 @@ def to_hex(a, b, c)
   end
 end
 
-def to_ints(hex)
-  r = hex[1..2]
-  g = hex[3..4]
-  b = hex[5..6]
-  [r, g, b].map { |h| h.hex }
-end
+def to_ints(hex) = hex.scan(/\w\w/).map(&:hex)
