ac5606a116ba56d738dd6bcbbb6ec933b7add3fd
nashcft <nashrfv@gmail.com>
2024-04-21 23:06:38 +0900

    chapter 5: add convert_length

---
+++ lib/convert_length.rb
@@ -0,0 +1,9 @@
+#  unit  | abbr. | metric equiv.
+#  ----- | ----- | --------------
+#  meter |     m |          1.00
+#   feet |    ft |          3.28
+#   inch |    in |         39.37
+
+def convert_length(length, unit_from, unit_to)
+  39.37
+end
