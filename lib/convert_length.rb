6561dc4840526584854927b7558069d989a9afd2
nashcft <nashrfv@gmail.com>
2024-04-21 23:12:25 +0900

    chapter 5: implement convert_length

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -5,5 +5,6 @@
 #   inch |    in |         39.37
 
 def convert_length(length, unit_from, unit_to)
-  39.37
+  units = { "m" => 1.0, "ft" => 3.28, "in" => 39.37 }
+  (length / units[unit_from] * units[unit_to]).round(2)
 end
