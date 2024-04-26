e1dec8d45ce8bf44c6500de6128fe0f5da11d7db
nashcft <nashrfv@gmail.com>
2024-04-26 17:00:53 +0900

    chapter 6: implement convert_hash_syntax

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -2,7 +2,25 @@ require "minitest/autorun"
 require_relative "../lib/convert_hash_syntax"
 
 class ConvertHashSyntaxTest < Minitest::Test
-  def test_convert_hash_syntax
+  def test_convert_hash_syntax_with_empty_hash
     assert_equal "{}", convert_hash_syntax("{}")
+  end
+
+  def test_convert_hash_syntax_with_non_empty_hash
+    input = <<~TEXT
+    {
+      :name => 'Alice',
+      :age=>20,
+      :gender   =>   :female
+    }
+    TEXT
+    expected = <<~TEXT
+    {
+      name: 'Alice',
+      age: 20,
+      gender: :female
+    }
+    TEXT
+    assert_equal expected, convert_hash_syntax(input)
   end
 end
