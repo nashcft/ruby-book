c86246bc2e1568206e61500e4e3a7aabbdc84eff
nashcft <nashrfv@gmail.com>
2024-05-03 01:18:50 +0900

    chapter 9: (extra) add no match test

--- test/regexp_checker.rb
+++ test/regexp_checker.rb
@@ -4,9 +4,9 @@ require_relative "../lib/regexp_checker"
 class RegexpCheckerTest < Minitest::Test
   def test_invalid_and_matched
     inputs = [
-      "123-456-789",
-      "[1-6+",
-      "[1-6]+"
+      "123-456-789\n",
+      "[1-6+\n",
+      "[1-6]+\n"
     ]
     expected = [
       "Text?: ",
@@ -14,6 +14,22 @@ class RegexpCheckerTest < Minitest::Test
       "Invalid pattern: premature end of char-class: /[1-6+/\n",
       'Pattern?: ',
       "Matched: 123, 456\n"
+    ].join
+
+    stub :gets, -> { inputs.shift } do
+      assert_output(expected) { regexp_checker_main }
+    end
+  end
+
+  def test_no_match
+    inputs = [
+      "abc-def-ghi\n",
+      "[1-6]+\n"
+    ]
+    expected = [
+      "Text?: ",
+      "Pattern?: ",
+      "Nothing matched.\n"
     ].join
 
     stub :gets, -> { inputs.shift } do
