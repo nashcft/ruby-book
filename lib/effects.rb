bc299e8d4a80eda12b30d84ac1a90f0c9c468e6a
nashcft <nashrfv@gmail.com>
2024-05-04 20:42:09 +0900

    chapter 10: implement Effects.echo

--- lib/effects.rb
+++ lib/effects.rb
@@ -2,4 +2,10 @@ module Effects
   def self.reverse
     lambda { |words| words.split(" ").map(&:reverse).join(" ") }
   end
+
+  def self.echo(rate)
+    lambda { |words|
+      words.each_char.map { |c| c == " " ? c : c * rate }.join
+    }
+  end
 end
