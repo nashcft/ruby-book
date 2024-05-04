0fdb6bcf7ee3a12487a8fc76b867b5dfcb3024bd
nashcft <nashrfv@gmail.com>
2024-05-04 20:46:05 +0900

    chapter 10: implement Effects.loud

--- lib/effects.rb
+++ lib/effects.rb
@@ -8,4 +8,10 @@ module Effects
       words.each_char.map { |c| c == " " ? c : c * rate }.join
     }
   end
+
+  def self.loud(level)
+    lambda { |words|
+      words.split(" ").map { |word| word.upcase + "!" * level }.join(" ")
+    }
+  end
 end
